df7e0f4ff55f65453b3e1532ad724477ea45ab46
Will Wernert <william.wernert@gmail.com>
2025-11-20 10:41:20 -0500

    fixup for new laptop

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 vim.opt.relativenumber = false
 vim.opt.foldcolumn = "0" -- Disable fold column clicks
+vim.opt.updatetime = 250 -- Increase update time
+vim.opt.swapfile = true
+vim.opt.directory = "/tmp//"
